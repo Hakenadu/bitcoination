59b0c260f5363ba7c6a94bfbcc7c9daae736391a
Manuel Seiche <hakenadu91@gmail.com>
2021-06-20 20:53:03 +0200

    #21

--- src/app/map/chart/map-chart.component.ts
+++ src/app/map/chart/map-chart.component.ts
@@ -93,8 +93,8 @@ export class MapChartComponent implements AfterViewInit {
       this.chart.smallMap.series.push(this.worldSeries);
 
       this.chart.zoomControl = new am4maps.ZoomControl();
-      this.chart.zoomControl.plusButton.tooltipText = 'zoom in';
-      this.chart.zoomControl.minusButton.tooltipText = 'zoom out';
+      this.chart.zoomControl.plusButton.tooltipText = 'Zoom in';
+      this.chart.zoomControl.minusButton.tooltipText = 'Zoom out';
       this.setButtonIconPath(this.chart.zoomControl.plusButton, 'M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14zm.5-7H9v2H7v1h2v2h1v-2h2V9h-2z');
       this.setButtonIconPath(this.chart.zoomControl.minusButton, 'M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14zM7 9h5v1H7z');
 
@@ -163,7 +163,7 @@ export class MapChartComponent implements AfterViewInit {
     homeButton.width = 30;
     homeButton.marginBottom = 3;
     homeButton.parent = chart.zoomControl;
-    homeButton.tooltipText = 'reset zoom';
+    homeButton.tooltipText = 'Reset zoom';
     homeButton.insertBefore(chart.zoomControl.plusButton);
   }
 
@@ -174,7 +174,7 @@ export class MapChartComponent implements AfterViewInit {
     toggleSmallMapButton.width = 30;
     toggleSmallMapButton.marginBottom = 3;
     toggleSmallMapButton.parent = chart.zoomControl;
-    toggleSmallMapButton.tooltipText = 'toggle minimap';
+    toggleSmallMapButton.tooltipText = 'Toggle minimap';
     toggleSmallMapButton.insertBefore(chart.zoomControl.plusButton);
   }
 
